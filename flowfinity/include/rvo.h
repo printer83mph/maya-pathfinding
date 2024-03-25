e8ffc570f1fd09891cf839ab3ac8869c1dc99fdb
Thomas Shaw <printer.83mph@gmail.com>
2024-03-24 21:12:17 -0400

    Complete RVO implementation I think

--- flowfinity/include/rvo.h
+++ flowfinity/include/rvo.h
@@ -4,24 +4,14 @@
 
 class VelocityObstacle {
 public:
-  VelocityObstacle(const glm::vec2 &pA, const glm::vec2 &pB,
-                   const glm::vec2 &vB, float radiusA, float radiusB);
+  VelocityObstacle(const glm::vec2 &pA, const glm::vec2 &vA,
+                   const glm::vec2 &pB, const glm::vec2 &vB, float radiusA,
+                   float radiusB);
 
-  bool isInVO(const glm::vec2 &velocityA) const;
+  bool isInVO(const glm::vec2 &newVelA) const;
+  bool isInRVO(const glm::vec2 &newVelA) const;
 
 private:
-  glm::vec2 m_posA, m_origin, m_ray;
+  glm::vec2 m_posA, m_velA, m_origin, m_ray;
   float m_halfConeAngle;
-};
-
-class ReciprovalVelocityObject {
-public:
-  ReciprovalVelocityObject(const glm::vec2 &pA, const glm::vec2 &vA,
-                           const glm::vec2 &pB, const glm::vec2 &vB,
-                           float radiusA, float radiusB);
-
-  bool isInRVO(const glm::vec2 velocityA) const;
-
-private:
-  VelocityObstacle m_aVO, m_bVO;
 };
--- flowfinity/src/rvo.cpp
+++ flowfinity/src/rvo.cpp
@@ -3,26 +3,17 @@
 #include "glm/geometric.hpp"
 #include "glm/trigonometric.hpp"
 
-VelocityObstacle::VelocityObstacle(const glm::vec2 &pA, const glm::vec2 &pB,
-                                   const glm::vec2 &vB, float radiusA,
-                                   float radiusB)
-    : m_posA(pA), m_origin(pA + vB), m_ray(glm::normalize(pB - pA)),
+VelocityObstacle::VelocityObstacle(const glm::vec2 &pA, const glm::vec2 &vA,
+                                   const glm::vec2 &pB, const glm::vec2 &vB,
+                                   float radiusA, float radiusB)
+    : m_posA(pA), m_velA(vA), m_origin(pA + vB), m_ray(glm::normalize(pB - pA)),
       m_halfConeAngle(glm::asin((radiusA + radiusB) / glm::length(pB - pA))) {}
 
-bool VelocityObstacle::isInVO(const glm::vec2 &velocityA) const {
-  return glm::dot(glm::normalize(m_posA + velocityA - m_origin), m_ray) >
+bool VelocityObstacle::isInVO(const glm::vec2 &newVelA) const {
+  return glm::dot(glm::normalize(m_posA + newVelA - m_origin), m_ray) >
          glm::cos(m_halfConeAngle);
 }
 
-ReciprovalVelocityObject::ReciprovalVelocityObject(const glm::vec2 &pA,
-                                                   const glm::vec2 &vA,
-                                                   const glm::vec2 &pB,
-                                                   const glm::vec2 &vB,
-                                                   float radiusA, float radiusB)
-    : m_aVO(pA, pB, vB, radiusA, radiusB), m_bVO(pB, pA, vA, radiusB, radiusA) {
-}
-
-bool ReciprovalVelocityObject::isInRVO(const glm::vec2 velocityA) const {
-  // TODO
-  return false;
+bool VelocityObstacle::isInRVO(const glm::vec2 &newVelA) const {
+  return isInVO(newVelA * 2.f - m_velA);
 }
