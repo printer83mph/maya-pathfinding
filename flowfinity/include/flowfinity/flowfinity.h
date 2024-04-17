fd44b0555298901a024d8a8cd04cdf70a04b6fa0
Thomas Shaw <printer.83mph@gmail.com>
2024-04-17 01:32:27 -0400

    Refactor flowfinity for swappable navigation methods

--- flowfinity/include/flowfinity/flowfinity.h
+++ flowfinity/include/flowfinity/flowfinity.h
@@ -1,17 +1,11 @@
 #pragma once
 
-#include "graph.h"
-#include "obstacle.h"
-
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/hash.hpp>
 #undef GLM_DISABLE_EXPERIMENTAL
-#include <glm/ext/vector_float2.hpp>
-#include <glm/ext/vector_float3.hpp>
 #include <glm/vec2.hpp>
 
 #include <array>
-#include <unordered_map>
 #include <vector>
 
 /**
@@ -34,16 +28,6 @@ public:
   void removeAgent(int index);
   void setAgentTarget(int index, const glm::vec2& target);
 
-  float getEdgeWeight(glm::vec2 point1, glm::vec2 point2);
-  void createGraph(const std::vector<Obstacle>& obstacles);
-  void addEndPoints(const std::vector<std::pair<glm::vec3, glm::vec3>>& endPoints,
-                    const std::vector<Obstacle>& obstacles);
-  const std::vector<std::pair<glm::vec2, glm::vec2>>& getEdges() const;
-
-  void clearEndPoints();
-
-  void getDisjkstraPaths(std::vector<std::vector<glm::vec3>>&);
-
 private:
   struct Config {
     float maxSpeed = 1.f;
@@ -60,17 +44,6 @@ private:
   std::array<glm::vec2, 33> m_possibleAccels;
 
   glm::vec2 findOptimalAcceleration(int index, float dt) const;
-
-  // Graph logic
-  int m_nextVertex;
-  Graph m_graph;
-  std::unordered_map<glm::vec3, int, std::hash<glm::vec3>> m_NodeToPoint;
-  std::unordered_map<int, glm::vec3> m_PointToNode;
-  std::vector<std::pair<glm::vec2, glm::vec2>> edges;
-  std::vector<int> m_waypoints;
-  std::vector<std::pair<int, int>> m_endPoints;
-
-  void addPoint(const glm::vec3& point);
 
   void addAgent(float x, float y);
   void addAgent(const glm::vec2& pos);
---
+++ flowfinity/include/flowfinity/navigation/navmethod.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <glm/vec2.hpp>
+#include <vector>
+
+class NavMethod
+{
+public:
+  virtual std::vector<glm::vec2> getPath(const glm::vec2& start, const glm::vec2& end) = 0;
+};
