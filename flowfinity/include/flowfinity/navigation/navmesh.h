2cebbf32a5af91ba8c8142b128b98737b3006993
Thomas Shaw <printer.83mph@gmail.com>
2024-04-17 21:34:26 -0400

    Beginnings of recast

---
+++ flowfinity/include/flowfinity/navigation/navmesh.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "navmethod.h"
+
+class NavMesh : public NavMethod
+{
+  NavMesh();
+  virtual ~NavMesh();
+};
---
+++ flowfinity/src/navigation/navmesh.cpp
@@ -0,0 +1,7 @@
+#include "flowfinity/navigation/navmesh.h"
+
+#include <recastnavigation/DetourNavMeshBuilder.h>
+
+NavMesh::NavMesh() {}
+
+NavMesh::~NavMesh() {}
